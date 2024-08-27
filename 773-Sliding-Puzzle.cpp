0380c0e16ee4b6153d0e5cb9fc1bbb2e6a7a8d54
ahmed0-07 <118305891+ahmed0-07@users.noreply.github.com>
2024-08-27 15:26:00 +0300

    [773] [Time Beats: 87.04%] [Memory Beats: 88.66%] - LeetPush

---
+++ 773-Sliding-Puzzle.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string ans = "123450";
+    vector<vector<int>> dir = {{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}}; //directions
+    int slidingPuzzle(vector<vector<int>>& board) {
+        string s = "";
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                s += board[i][j] + '0';
+            }
+        }
+
+        if(s == ans)
+            return 0;
+        queue<string> q;
+        unordered_set<string> se; //to keep track not to go with a old value we already accessed
+        q.push(s);
+
+        for(int level = 0, sz = q.size(); !q.empty(); level++, sz = q.size())
+        {
+            while(sz--)
+            {
+                string cur = q.front();
+                q.pop();
+
+                int idx = cur.find('0');
+
+                for(int i : dir[idx])
+                {
+                    swap(cur[i], cur[idx]);
+
+                    if(cur == ans)
+                        return level + 1;
+
+                    if(!se.count(cur))
+                    {
+                        se.insert(cur);
+                        q.push(cur);
+                    }
+
+                    swap(cur[i], cur[idx]); //undo
+                }
+            }
+        }
+        return -1;
+    }
+};
