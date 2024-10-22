33a58cb620453201c48262ab0a26c67fa3319f8b
ahmed0-07 <118305891+ahmed0-07@users.noreply.github.com>
2024-10-23 02:23:45 +0300

    [91] [Time Beats: 100.00%] [Memory Beats: 32.32%] - LeetPush

--- 91-Decode-Ways.cpp
+++ 91-Decode-Ways.cpp
@@ -2,37 +2,31 @@ class Solution {
 public:
     int dp[105];
     string s;
-    int f(int idx, string ans)
+    int f(int idx)
     {
-        if(ans.size() == s.size())
+        if(idx == s.size())
             return 1;
-        else if(idx >= s.size())
+        
+        if(s[idx] == '0')
             return 0;
 
         if(dp[idx] != -1)
             return dp[idx];
 
         int ch1 = 0, ch2 = 0;
-        if(s[idx] != '0')
-        {
-            ans += s[idx];
-            ch1 += f(idx+1, ans);
-        }
+        ch1 = f(idx+1);
         
         string x = \\;
         x +=s[idx];
         x +=s[idx+1];
-        if(s[idx] != '0' && idx+1 < s.size() && x <= \26\)
-        {
-            ans += s[idx+2];
-            ch2 += f(idx+2, ans);
-        }
+        if(idx+1 < s.size() && x >= \1\ && x <= \26\)
+            ch2 += f(idx+2);
 
         return dp[idx] = ch1 + ch2;
     }
     int numDecodings(string t) {
         s = t;
         memset(dp, -1, sizeof(dp));
-        return f(0, \\);
+        return f(0);
     }
 };
